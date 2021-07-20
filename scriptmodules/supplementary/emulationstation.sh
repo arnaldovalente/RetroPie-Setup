0b4b766f79b9a394806c3037c7192c61f39b3a6a
arnaldovalente <37136367+arnaldovalente@users.noreply.github.com>
2021-07-20 17:12:19 +0200

    Update emulationstation.sh

--- scriptmodules/supplementary/emulationstation.sh
+++ scriptmodules/supplementary/emulationstation.sh
@@ -171,6 +171,7 @@ function build_emulationstation() {
         [[ "$gl_ver" -gt 1 ]] && params+=(-DUSE_OPENGL_21=On)
     elif isPlaftorm "armbian"; then
         params+=(-DGLES=On)
+        params+=(-DUSE_GLES1=On)
     fi
     rpSwap on 1000
     cmake . "${params[@]}"
